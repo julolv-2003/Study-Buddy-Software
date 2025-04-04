02a06f4de90329e54729b06935711e1be86821ba
julolv-2003 <jolvera2003@gmail.com>
2025-04-03 20:05:13 -0700

    Created class UserPersonalAccount with default information.

---
+++ Software/user.py
@@ -0,0 +1,43 @@
+import json
+
+class UserPersonalAccount:
+    def UpdateUserInfo():
+        return
+
+    def __init__(self, username, password, profile_picture, profile_description, profile_attributes, blocked_by, blocked_users):
+        self.username = username
+        self.password = password
+        self.profile_picture = profile_picture
+        self.profile_description = profile_description
+        self.profile_attributes = profile_attributes
+        self.blocked_by = blocked_by
+        self.blocked_users = blocked_users
+
+    def SetProfilePicture():
+        return
+    
+    def SetProfileAttributes():
+        return
+    
+    def SetProfileDescription():
+        return
+    
+    def ChangeUsername():
+        return
+    
+    def ChangePassword():
+        return
+    
+    def BlockUser():
+        return
+    
+    def UnblockUser():
+        return
+    
+    def SaveUserData(self):
+        return
+
+
+
+
+    
